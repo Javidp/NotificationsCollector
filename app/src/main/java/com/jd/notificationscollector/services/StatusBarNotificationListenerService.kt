a79f8d96a6e54daf8823f588d3fa4065af7d9e58
Javidp <dawid.cpp@gmail.com>
2021-03-24 23:02:40 +0100

    Add support for saving notifications while database is locked

--- app/src/main/java/com/jd/notificationscollector/services/StatusBarNotificationListenerService.kt
+++ app/src/main/java/com/jd/notificationscollector/services/StatusBarNotificationListenerService.kt
@@ -1,5 +1,7 @@
 package com.jd.notificationscollector.services
 
+import android.database.sqlite.SQLiteDatabaseLockedException
+import android.database.sqlite.SQLiteTableLockedException
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
@@ -8,12 +10,20 @@ import com.jd.notificationscollector.database.NcDatabase
 import com.jd.notificationscollector.model.AppInfo
 import com.jd.notificationscollector.model.Notification
 import com.jd.notificationscollector.model.NotificationLog
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private const val MAX_WAITING_TIME = 300_000L
+private const val NEXT_CHECK_DELAY_TIME = 10_000L
 
 class StatusBarNotificationListenerService: NotificationListenerService() {
 
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         try {
-            if ((sbn?.notification?.flags ?: 0x0) and android.app.Notification.FLAG_GROUP_SUMMARY != 0) return
+            if ((sbn?.notification?.flags ?: 0x0) and android.app.Notification.FLAG_GROUP_SUMMARY != 0) {
+                return
+            }
             val packageName = sbn?.packageName ?: return
 
             val db = NcDatabase.create(applicationContext)
@@ -29,39 +39,80 @@ class StatusBarNotificationListenerService: NotificationListenerService() {
                 applicationContext.createPackageContext(sbn.packageName, 0).resources.getDrawable(it, applicationContext.theme)
             }
 
-            val appInfo = applicationContext.packageManager.getApplicationInfo(sbn.packageName, 0)
-            val appName = applicationContext.packageManager.getApplicationLabel(appInfo).toString()
-            val appIcon = applicationContext.packageManager.getApplicationIcon(sbn.packageName)
+            val applicationInfo = applicationContext.packageManager.getApplicationInfo(sbn.packageName, 0)
+            val applicationName = applicationContext.packageManager.getApplicationLabel(applicationInfo).toString()
+            val applicationIcon = applicationContext.packageManager.getApplicationIcon(sbn.packageName)
 
             val bitmapDrawableConverter = BitmapDrawableConverter(this)
-            sbn.packageName?.let {
-                db.appsInfoDao().insertIfNotExists(AppInfo(it, appName, bitmapDrawableConverter.toByteArray(appIcon), true))
-            }
-            var notificationIconBlob: ByteArray? = null
-            notificationIcon?.let {
-                notificationIconBlob = bitmapDrawableConverter.toByteArray(notificationIcon)
-            }
-            val savedNotificationId = db.notificationsDao().insert(
-                Notification(
-                    title.toString(),
-                    text.toString(),
-                    bigText.toString(),
-                    sbn.packageName,
-                    sbn.postTime,
-                    notificationIconBlob,
-                    color
+
+            val applicationIconBlob = bitmapDrawableConverter.toByteArray(applicationIcon)
+            val appInfo = sbn.packageName?.let { itPackageName ->
+                AppInfo(
+                    itPackageName,
+                    applicationName,
+                    applicationIconBlob,
+                    true
                 )
+            }
+
+            val notificationIconBlob = notificationIcon?.let {
+                bitmapDrawableConverter.toByteArray(notificationIcon)
+            }
+            val notification = Notification(
+                title.toString(),
+                text.toString(),
+                bigText.toString(),
+                sbn.packageName,
+                sbn.postTime,
+                notificationIconBlob,
+                color
             )
-            db.notificationsLogsDao().insert(
-                NotificationLog(
-                    savedNotificationId,
-                    sbn.toString(),
-                    sbn.notification.toString(),
-                    sbn.notification?.extras.toString()
-                )
+            val notificationLog = NotificationLog(
+                0,
+                sbn.toString(),
+                sbn.notification.toString(),
+                sbn.notification?.extras.toString()
             )
+
+            save(db, appInfo, notification, notificationLog)
         } catch (e: Exception) {
             Log.e(javaClass.simpleName, "Exception during saving notification", e)
+        }
+    }
+
+    private fun save(db: NcDatabase, appInfo: AppInfo?, notification: Notification, notificationLog: NotificationLog) {
+        GlobalScope.launch {
+            val expireTime = System.currentTimeMillis() + MAX_WAITING_TIME
+
+            while (System.currentTimeMillis() < expireTime) {
+                try {
+                    db.runInTransaction {
+                        appInfo?.let { db.appsInfoDao().insertIfNotExists(it) }
+
+                        val savedNotificationId = db.notificationsDao().insert(notification)
+
+                        notificationLog.notificationId = savedNotificationId
+                        db.notificationsLogsDao().insert(notificationLog)
+                    }
+                    break
+                } catch (e: java.lang.Exception) {
+                    when (e) {
+                        is SQLiteDatabaseLockedException,
+                        is SQLiteTableLockedException -> {
+                            if (System.currentTimeMillis() < expireTime) {
+                                delay(NEXT_CHECK_DELAY_TIME)
+                                continue
+                            } else {
+                                break
+                            }
+                        }
+                        else -> {
+                            Log.e(javaClass.simpleName, "Unknown exception during saving notification", e)
+                            break
+                        }
+                    }
+                }
+            }
         }
     }
 
